16eb3cc3363e6047dc570c4022cfa5c332170e18
José Diogo Nápoles de Sousa Rente <1211155@isep.ipp.pt>
2023-02-24 20:47:36 +0000

    exercicios 5 e 7

---
+++ sprint1/processos/5/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() {
+    pid_t child1, child2;
+    int status1, status2;
+
+    child1 = fork();
+    if (child1 == 0) {
+        printf("[A EXECUTAR] Filho 1 com PID %d\n", getpid());
+        sleep(1);
+        printf("\n[FIM DE EXECUÇÃO] Filho 1 com PID %d\n", getpid());
+        exit(1);
+    } else if (child1 == -1) {
+    	perror("Fork falhou");
+		exit(1);
+    }
+
+    child2 = fork();
+    if (child2 == 0) {
+        printf("[A EXECUTAR] Filho 2 com PID %d\n", getpid());
+        sleep(2);
+        printf("\n[FIM DE EXECUÇÃO] Filho 2 com PID %d\n", getpid());
+        exit(2);
+    } else if (child2 == -1) {
+    	perror("Fork falhou");
+		exit(1);
+    }
+
+    // wait for both processes to finish
+    waitpid(child1, &status1, 0);
+    printf("O processo filho com PID %d retornou: %d\n", child1, WEXITSTATUS(status1));
+    
+    waitpid(child2, &status2, 0);
+    printf("O processo filho com PID %d retornou: %d\n", child2, WEXITSTATUS(status2));
+    return 0;
+}
+
+
---
+++ sprint1/processos/7/main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+#define ARRAY_SIZE 1000
+
+int main() {
+	int numbers[ARRAY_SIZE];  /* array to lookup */
+	int n;        /* the number to find */
+	time_t t;        /* needed to initialize random number generator (RNG) */
+	int i, count = 0, status;
+
+	/* intializes RNG (srand():stdlib.h; time(): time.h) */
+	srand ((unsigned) time (&t));
+
+	/* initialize array with random numbers (rand(): stdlib.h) */
+	for (i = 0; i < ARRAY_SIZE; i++)
+		numbers[i] = rand () % 10000;
+
+	/* initialize n */
+	n = rand () % 10000;
+	
+	printf("Número de vezes que %d aparece no array \n", n);
+	pid_t pid = fork();
+
+	if (pid == 0) {  // child process 
+		int child_count = 0;
+		for (i = 0; i < ARRAY_SIZE / 2; i++) {
+			if (numbers[i] == n) {
+				child_count++;
+			}
+		}
+		printf("[Processo filho]: %dx\n", child_count);
+		exit(0);
+	} else if (pid > 0) {  // parent process
+		int parent_count = 0;
+		for (i = ARRAY_SIZE / 2; i < ARRAY_SIZE; i++) {
+			if (numbers[i] == n) {
+				parent_count++;
+			}
+		}
+		printf("[Processo pai]: %dx\n", parent_count);
+		wait(&status);  // wait for child process to finish
+
+		if (WIFEXITED(status)) {  // check if child process exited normally 
+			count = WEXITSTATUS(status) + parent_count;
+			printf("[TOTAL]: %dx\n", count);
+		}
+
+	} else {  // error
+		perror("Fork falhou");
+		exit(1);
+	}
+
+	return 0;
+}
+
+
+
