3ef1c2ce89b16bcd8d2ff9b1a0ef5fba825d85ad
José Diogo Nápoles de Sousa Rente <1211155@isep.ipp.pt>
2023-03-29 21:25:07 +0100

    signals: ex15

---
+++ sprint1/sinais/ex15/main.c
@@ -0,0 +1,58 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+typedef struct {
+  char cmd[32];
+  int time_cap;
+} command_t;
+
+void handle_SIGALRM(int signo) {
+  write(STDOUT_FILENO, "The command did not complete in its allowed time!\n",
+        49);
+}
+
+void handle_SIGCHLD(int signo) {
+  write(STDOUT_FILENO, "Execution of the command ended!\n", 32);
+}
+
+int main(void) {
+  command_t commands[] = {{"ls", 10}, {"cd", 3}};
+  int num_commands = sizeof(commands) / sizeof(commands[0]);
+
+  struct sigaction act1, act2;
+  memset(&act1, 0, sizeof(struct sigaction));
+  memset(&act2, 0, sizeof(struct sigaction));
+
+  act1.sa_handler = handle_SIGALRM;
+  act2.sa_handler = handle_SIGCHLD;
+
+  sigemptyset(&act1.sa_mask);
+  sigemptyset(&act2.sa_mask);
+
+  sigaction(SIGALRM, &act1, NULL);
+  sigaction(SIGCHLD, &act2, NULL);
+
+  pid_t pid;
+  int status;
+  for (int i = 0; i < num_commands; i++) {
+    pid = fork();
+    if (pid == 0) { // child process
+      if (execvp(commands[i].cmd, (char *[]){commands[i].cmd, NULL}) == -1) {
+        printf("Failed to execute command: %s\n", commands[i].cmd);
+        exit(EXIT_FAILURE);
+      }
+    } else if (pid > 0) { // parent process
+      alarm(commands[i].time_cap);
+      wait(&status);
+    } else { // error
+      perror("Failed to fork");
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  return 0;
+}
