7b6b02fe7bfe34267abe6da7d707bb38298ecc59
José Diogo Nápoles de Sousa Rente <1211155@isep.ipp.pt>
2023-03-31 19:15:28 +0100

    sprint1: reformat

--- sprint1/processos/ex07/main.c
+++ sprint1/processos/ex07/main.c
@@ -6,55 +6,52 @@
 #define ARRAY_SIZE 1000
 
 int main() {
-	int numbers[ARRAY_SIZE];  /* array to lookup */
-	int n;        /* the number to find */
-	time_t t;        /* needed to initialize random number generator (RNG) */
-	int i, count = 0, status;
+  int numbers[ARRAY_SIZE]; /* array to lookup */
+  int n;                   /* the number to find */
+  time_t t; /* needed to initialize random number generator (RNG) */
+  int i, count = 0, status;
 
-	/* intializes RNG (srand():stdlib.h; time(): time.h) */
-	srand ((unsigned) time (&t));
+  /* intializes RNG (srand():stdlib.h; time(): time.h) */
+  srand((unsigned)time(&t));
 
-	/* initialize array with random numbers (rand(): stdlib.h) */
-	for (i = 0; i < ARRAY_SIZE; i++)
-		numbers[i] = rand () % 10000;
+  /* initialize array with random numbers (rand(): stdlib.h) */
+  for (i = 0; i < ARRAY_SIZE; i++)
+    numbers[i] = rand() % 10000;
 
-	/* initialize n */
-	n = rand () % 10000;
-	
-	printf("Número de vezes que %d aparece no array \n", n);
-	pid_t pid = fork();
+  /* initialize n */
+  n = rand() % 10000;
 
-	if (pid == 0) {  // child process 
-		int child_count = 0;
-		for (i = 0; i < ARRAY_SIZE / 2; i++) {
-			if (numbers[i] == n) {
-				child_count++;
-			}
-		}
-		printf("[Processo filho]: %dx\n", child_count);
-		exit(0);
-	} else if (pid > 0) {  // parent process
-		int parent_count = 0;
-		for (i = ARRAY_SIZE / 2; i < ARRAY_SIZE; i++) {
-			if (numbers[i] == n) {
-				parent_count++;
-			}
-		}
-		printf("[Processo pai]: %dx\n", parent_count);
-		wait(&status);  // wait for child process to finish
+  printf("Número de vezes que %d aparece no array \n", n);
+  pid_t pid = fork();
 
-		if (WIFEXITED(status)) {  // check if child process exited normally 
-			count = WEXITSTATUS(status) + parent_count;
-			printf("[TOTAL]: %dx\n", count);
-		}
+  if (pid == 0) { // child process
+    int child_count = 0;
+    for (i = 0; i < ARRAY_SIZE / 2; i++) {
+      if (numbers[i] == n) {
+        child_count++;
+      }
+    }
+    printf("[Processo filho]: %dx\n", child_count);
+    exit(0);
+  } else if (pid > 0) { // parent process
+    int parent_count = 0;
+    for (i = ARRAY_SIZE / 2; i < ARRAY_SIZE; i++) {
+      if (numbers[i] == n) {
+        parent_count++;
+      }
+    }
+    printf("[Processo pai]: %dx\n", parent_count);
+    wait(&status); // wait for child process to finish
 
-	} else {  // error
-		perror("Fork falhou");
-		exit(1);
-	}
+    if (WIFEXITED(status)) { // check if child process exited normally
+      count = WEXITSTATUS(status) + parent_count;
+      printf("[TOTAL]: %dx\n", count);
+    }
 
-	return 0;
+  } else { // error
+    perror("Fork falhou");
+    exit(1);
+  }
+
+  return 0;
 }
-
-
-
--- sprint1/processos/ex11/main.c
+++ sprint1/processos/ex11/main.c
@@ -6,73 +6,73 @@
 #define ARRAY_SIZE 1000
 
 int main() {
-    int numbers[ARRAY_SIZE], result[ARRAY_SIZE], max_value[NUM_CHILDREN], max = 0;
+  int numbers[ARRAY_SIZE], result[ARRAY_SIZE], max_value[NUM_CHILDREN], max = 0;
 
-    /* initialize array with random numbers (rand(): stdlib.h) */
-    for (int i = 0; i < ARRAY_SIZE; i++){
-        numbers[i] = rand() % 256;    
-    }
+  /* initialize array with random numbers (rand(): stdlib.h) */
+  for (int i = 0; i < ARRAY_SIZE; i++) {
+    numbers[i] = rand() % 256;
+  }
 
-    // Find maximum value of 1/5 of the array
-    for (int i = 0; i < NUM_CHILDREN; i++) {
-        pid_t pid = fork();
-        if (pid == 0) { //child process
-            int child_max = 0;
-            int start_index = i * ARRAY_SIZE / NUM_CHILDREN;
-            int end_index = (i + 1) * ARRAY_SIZE / NUM_CHILDREN;
-            for (int j = start_index; j < end_index; j++) {
-                if (numbers[j] > child_max) {
-                    child_max = numbers[j];
-                }
-            }
-            exit(child_max);
-        } else if (pid < 0) {
-            perror("Fork failed!");
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    // Wait for all child processes to finish
-    for (int i = 0; i < NUM_CHILDREN; i++) {
-        int status;
-        wait(&status);
-        max_value[i] = WEXITSTATUS(status);
-    }
-
-    // Compute maximum value of the entire array
-    for(int i = 0; i < NUM_CHILDREN; i++){
-        printf("-> Maximum value of the array(%d/%d): %d\n", (i+1), NUM_CHILDREN, max_value[i]);
-        if(max_value[i] > max) max = max_value[i];
-    }
-    printf("-> Maximum value of the array: %d\n", max);
-
-    //=============================================
-
-    // Compute results for first half of array using child process
+  // Find maximum value of 1/5 of the array
+  for (int i = 0; i < NUM_CHILDREN; i++) {
     pid_t pid = fork();
-    if (pid == 0) {
-        // Child process computes result for first half of array
-        for (int i = 0; i < ARRAY_SIZE / 2; i++) {
-            result[i] = ((int) numbers[i] / max) * 100;
-            printf("[CHILD] Result for index %d: %d\n", i, result[i]);
+    if (pid == 0) { // child process
+      int child_max = 0;
+      int start_index = i * ARRAY_SIZE / NUM_CHILDREN;
+      int end_index = (i + 1) * ARRAY_SIZE / NUM_CHILDREN;
+      for (int j = start_index; j < end_index; j++) {
+        if (numbers[j] > child_max) {
+          child_max = numbers[j];
         }
-        exit(EXIT_SUCCESS);
+      }
+      exit(child_max);
     } else if (pid < 0) {
-        perror("Fork failed!");
-        exit(EXIT_FAILURE);
+      perror("Fork failed!");
+      exit(EXIT_FAILURE);
     }
+  }
 
-    // Compute results for second half of array using parent process
-    for (int i = ARRAY_SIZE / 2; i < ARRAY_SIZE; i++) {
-        result[i] = ((int) numbers[i] / max) * 100;
+  // Wait for all child processes to finish
+  for (int i = 0; i < NUM_CHILDREN; i++) {
+    int status;
+    wait(&status);
+    max_value[i] = WEXITSTATUS(status);
+  }
+
+  // Compute maximum value of the entire array
+  for (int i = 0; i < NUM_CHILDREN; i++) {
+    printf("-> Maximum value of the array(%d/%d): %d\n", (i + 1), NUM_CHILDREN,
+           max_value[i]);
+    if (max_value[i] > max)
+      max = max_value[i];
+  }
+  printf("-> Maximum value of the array: %d\n", max);
+
+  //=============================================
+
+  // Compute results for first half of array using child process
+  pid_t pid = fork();
+  if (pid == 0) {
+    // Child process computes result for first half of array
+    for (int i = 0; i < ARRAY_SIZE / 2; i++) {
+      result[i] = ((int)numbers[i] / max) * 100;
+      printf("[CHILD] Result for index %d: %d\n", i, result[i]);
     }
-    wait(NULL);
+    exit(EXIT_SUCCESS);
+  } else if (pid < 0) {
+    perror("Fork failed!");
+    exit(EXIT_FAILURE);
+  }
 
-    for (int i = ARRAY_SIZE / 2; i < ARRAY_SIZE; i++) {
-        printf("[PARENT] Result for index %d: %d\n", i, result[i]);
-    }
+  // Compute results for second half of array using parent process
+  for (int i = ARRAY_SIZE / 2; i < ARRAY_SIZE; i++) {
+    result[i] = ((int)numbers[i] / max) * 100;
+  }
+  wait(NULL);
 
-    return 0;
+  for (int i = ARRAY_SIZE / 2; i < ARRAY_SIZE; i++) {
+    printf("[PARENT] Result for index %d: %d\n", i, result[i]);
+  }
+
+  return 0;
 }
-
-
